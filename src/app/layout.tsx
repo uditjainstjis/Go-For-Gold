a5afef8e5c642a6189fa53d19fabaa14772b7b70
Udit Jain <uditjainstjis@gmail.com>
2025-04-23 21:51:22 +0530

    asdfdasf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import type { Metadata } from "next";
+import Head from 'next/head';
+
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
@@ -13,13 +14,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Go For Gold",
-  description: "Powered by Newton School Of Technology",
-  icons: {
-    icon: '/logo-real.ico',
-  },
-};
 
 export default function RootLayout({
   children,
@@ -30,7 +24,18 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Favicon */}
+        <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Powered by Newton School Of Technology"
+        />
+        <meta
+          name="title"
+          content="Go For Gold"
+        />
         <link rel="icon" type="image/x-icon" href="/logo-real.ico"  />
+        </Head>
 
         {/* Google Analytics */}
         <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-1QLBB1PD6J" />
